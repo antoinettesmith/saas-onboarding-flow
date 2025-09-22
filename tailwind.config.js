08db93c04619bd166d67f7f8db22736dcd3311bf
Antoinette Smith <149444116+antoinettesmith@users.noreply.github.com>
2025-09-21 23:53:41 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       spacing: {
-        7: "1.75rem"
+        7: "1.75rem",
       },
       height: {
-        128: "280px"
-      }
-    }
+        128: "280px",
+      },
+    },
   },
-  variants: {},
-  plugins: []
+  plugins: [],
 };
